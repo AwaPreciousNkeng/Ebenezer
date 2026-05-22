dcb9c3f0933588373af5af06af0af126682934ab
AwaPreciousNkeng <awaprecious3t@gmail.com>
2026-05-22 05:32:29 +0100

    Init Ebenezer

---
+++ src/test/java/com/codewithpcodes/ebenezer/EbenezerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithpcodes.ebenezer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbenezerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
